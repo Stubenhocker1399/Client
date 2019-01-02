eafdc1abe430a24ba072e632390646cd0e41caa0
Emma3DSS <43555971+Emma3DSS@users.noreply.github.com>
2019-01-02 18:28:09 +1000

    HAND UI WORKS
    YAY

---
+++ Assets/Scripts/ChangeHandToLeft.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeHandToLeft : MonoBehaviour
+{
+
+    public Sprite LeftDefault;
+    public Sprite LeftMain;
+    public Sprite RightDefault;
+    public Sprite RightMain;
+    public GameObject LeftHand;
+
+
+    public void SetHighlightedLeft()
+    {
+
+        gameObject.GetComponent<Image>().sprite = RightDefault;
+        LeftHand.GetComponent<Image>().sprite = LeftMain;
+
+    }
+
+
+}
