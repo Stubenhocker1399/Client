ad96be8ed48bda432cba4b73d03a08f640843dc8
Emma3DSS <43555971+Emma3DSS@users.noreply.github.com>
2019-01-03 20:41:19 +1000

    Added a crowbar
    ;^)

--- Assets/Characters/Scripts/CharacterMovement.cs
+++ Assets/Characters/Scripts/CharacterMovement.cs
@@ -26,6 +26,7 @@ public class CharacterMovement : MonoBehaviour
         var input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
         currentDirection = Vector2.MoveTowards(currentDirection, input, acceleration * Time.deltaTime);
         
+
         if(currentDirection.magnitude > 0)
         {
             // Move relative to the camera
